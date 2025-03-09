32f340f58338fade211d6f0e7e917114c5366172
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-09 14:11:17 -0600

    Capitulo 12
    remove(valor): Se usa para eliminar un elemento específico de la lista por su nombre en lugar de su índice.
    
    Eliminaciones:
    
        colores.remove('amarillo') elimina 'amarillo'.
        colores.remove('rojo') elimina 'rojo'.
    
    print(colores): Se imprime la lista sin los elementos eliminados.

---
+++ 0012_Practica1_Listas_Remove.py
@@ -0,0 +1,8 @@
+# Definir una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+# Eliminar el color 'amarillo' de la lista
+colores.remove('amarillo')
+# Eliminar el color 'rojo' de la lista
+colores.remove('rojo')
+# Imprimir la lista después de las eliminaciones
+print(colores)
