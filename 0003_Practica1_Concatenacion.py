4eaf471a1333a05cad9419c56585cdd97e8982bd
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-09 11:54:11 -0600

    Capitulo 3
    Concatenación en una variable: Se combinan dos cadenas y se guardan en variable1.
    
    Concatenación de variables: Se unen variable2 y variable3 en variable4.
    Concatenación en print(): Se concatenan nombre y un string directamente en un print().
    
    Concatenación de nombre y apellidos: Se unen nombre y apellidos con un espacio entre ellos y se muestran.
    
    Concatenación de números como texto: Se unen dos números como cadenas y se imprime "1010".

---
+++ 0003_Practica1_Concatenacion.py
@@ -0,0 +1,15 @@
+variable1="Este es un "+"codigo de como concatenar"
+print(variable1)
+variable2="Hola mundo"
+variable3=" este es un codigo en python"
+variable4=variable2+variable3
+print(variable4)
+
+nombre="Josue Alejandro"
+apellidos=" Gomez Soltero"
+variable5=nombre+apellidos
+print(variable5)
+
+numero1="10"
+numero2="10"
+print(numero1+numero2)
