e61bfc4546fbc0fc248970c3841251b5dc2a3db1
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-09 12:41:51 -0600

    Capitulo 4
    Formatear el nombre: Se convierte el nombre completo a formato título, donde la primera letra de cada palabra es mayúscula usando el método title.
    
    Convertir a minúsculas: Se convierte toda la frase mensaje1 a minúsculas con el método lower().
    
    Convertir a mayúsculas: Se convierte mensaje2 a mayúsculas con el método upper().

---
+++ 0004_Practica1_Upper_Lower_Title.py
@@ -0,0 +1,21 @@
+# Definir una variable con el nombre completo
+nombre= "josue alejandro gomez soltero"
+# Aplicar el método title() para poner la primera letra de cada palabra en mayúscula
+nombre = nombre.title()
+
+# Imprimir el nombre con el formato cambiado
+print(nombre)
+# Definir una variable con una frase
+mensaje1="Esta Es Una Frase Para Ser Formateada."
+# Convertir toda la frase a minúsculas usando lower()
+mensaje1=mensaje1.lower()
+# Imprimir la frase en minúsculas
+print(mensaje1)
+
+# Definir otra variable con una frase
+mensaje2="Esta Es Una Frase Para Ser Formateada."
+
+# Convertir la frase a mayúsculas usando upper()
+mensaje2=mensaje2.upper()
+# Imprimir la frase en mayúsculas
+print(mensaje2)
