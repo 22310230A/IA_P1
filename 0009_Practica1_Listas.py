ac0d28e5703fb0a0d88d5ce4dd649c66edcee47c
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-09 13:49:50 -0600

    Capitulo 9
    Listas ([]): Se almacenan elementos en una estructura de datos ordenada.
    
    Índices positivos (0, 3): Se accede a elementos específicos de la lista por su posición.
    
    Índices negativos (-1): Se usa para acceder al último elemento de la lista.
    
    Impresión de listas (print()): Se muestra la lista completa o elementos individuales.

---
+++ 0009_Practica1_Listas.py
@@ -0,0 +1,10 @@
+# Definir una lista de colores
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+# Salida: amarillo
+print(colores[3])
+# Salida: rojo rosa
+print(colores[0],colores[-1])
+# Definir una lista de números escritos como cadenas de texto
+lista =["tres","dos","cinco","cuatro","uno"]
+ # Salida: ['tres', 'dos', 'cinco', 'cuatro', 'uno']
+print(lista)
