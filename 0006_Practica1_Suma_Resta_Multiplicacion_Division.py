32e507e54b4804c79c70434a08bd18a4d1b5a51d
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-09 13:30:54 -0600

    Capitulo 6
    El código realiza diferentes operaciones matemáticas utilizando los siguientes operadores:
    
    Suma (+): Se usa para agregar valores.
    
    Resta (-): Se usa para restar valores.
    
    Multiplicación (*): Se usa para multiplicar valores.
    
    División (/): Se usa para dividir valores.
    
    Operación combinada: Usa suma, resta, multiplicación y división siguiendo la jerarquía de operaciones.

---
+++ 0006_Practica1_Suma_Resta_Multiplicacion_Division.py
@@ -0,0 +1,23 @@
+# Definir dos números
+numero1=20
+numero2=23
+# Realizar una suma y almacenarla en la variable 'suma'
+suma=numero1+numero2+44
+ # Imprimir el resultado de la suma
+print(suma)
+# Realizar una resta y almacenarla en la variable 'resta'
+resta=numero1-numero2-84
+# Imprimir el resultado de la resta
+print(resta)
+# Realizar una multiplicación y almacenarla en la variable 'multiplicacion'
+multiplicacion=numero1*numero2*1.89
+ # Imprimir el resultado de la multiplicación
+print(multiplicacion)
+# Realizar una división y almacenarla en la variable 'division'
+division=5000/230/2.17
+ # Imprimir el resultado de la división
+print(division)
+#Realizar una operación combinada y almacenarla en la variable 'operacion'
+operacion=10 / 5 +15 -17 *1
+# Imprimir el resultado de la operación
+print(operacion)
