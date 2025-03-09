d83df91e92de70555ae7b4ba6c77a7ffe0e32e40
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-09 11:29:05 -0600

    Capitulo 2
    El código define tres variables de tipo cadena. Las dos primeras (variable1 y variable2) almacenan el mismo mensaje "Hola mundo", pero con diferentes tipos de comillas (dobles y simples). La tercera (variable3) contiene una explicación sobre la función print(). Finalmente, el código muestra el contenido de variable3 en la consola usando print().

---
+++ 0002_Practica1_Strings.py
@@ -0,0 +1,11 @@
+# Definir una variable con un mensaje usando comillas dobles
+variable1=("Hola mundo")
+
+# Definir otra variable con el mismo mensaje pero usando comillas simples
+variable2 = ('Hola mundo')
+
+# Definir una variable con una cadena explicativa sobre print()
+variable3=('"print()" se utiliza para imprimir valores en la consola.')
+
+# Imprimir el contenido de variable3 en la consola
+print(variable3)
