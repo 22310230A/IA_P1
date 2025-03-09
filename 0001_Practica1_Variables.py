994e4b97c2feb2f903e9f6fef0ba26cef48106b7
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-09 11:23:20 -0600

    Capitulo 1
    El código primero guarda un mensaje de texto en una variable y lo muestra en pantalla con print(). Luego, define dos variables numéricas, las suma y guarda el resultado en otra variable. Finalmente, usa print() nuevamente para mostrar el resultado de la suma.

---
+++ 0001_Practica1_Variables.py
@@ -0,0 +1,12 @@
+#Variables 
+# Guardamos un mensaje en una variable
+mensaje=("Hola mundo")
+# Mostramos el mensaje en pantalla
+print(mensaje)
+# Definimos dos números
+variable1=65
+variable2=35
+# Sumamos los dos números y lo guardamos en una variable
+variable3=variable1+variable2
+# Mostramos el resultado de la suma
+print(variable3)
