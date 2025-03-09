0992fce0a38787ef117ba5f591a4c20dded3f385
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-09 13:55:34 -0600

    Capitulo 10
    Listas ([]): Se usa una lista para almacenar los colores.
    
    Índices negativos (-1, -7, -5, -2, -10): Se utilizan para acceder a los elementos desde el final de la lista.
    
        colores[-1] → "naranja"
        colores[-7] → "amarillo"
        colores[-5] → "lila"
        colores[-2] → "blanco"
        colores[-10] → "rojo"
    
    Uso de print(): Se imprimen los colores en una sola línea.

---
+++ 0010_Practica1_Listas_Negativas.py
@@ -0,0 +1,5 @@
+# Definir una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+# Acceder e imprimir los colores usando índices negativos
+print(colores[-1],colores[-7],colores[-5],colores[-2],colores[-10])
+# Salida: naranja amarillo lila blanco rojo
