034f96dfb3921cceca5c85e9c3b7a3af59d2a7cf
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:11:49 -0600

    Capitulo 18
    Tuplas (() en lugar de []): Se definen listas inmutables de valores.
    
    colores[-1]: Se accede al último elemento de la tupla.
    
    operacion: Se realiza una operación matemática con valores de la tupla numeros:
    
        10 + 5 - 1 * 11 → 10 + 5 - 11 → 4.
    
    print(operacion) muestra el resultado de la operación.

---
+++ 0018_Practica1_Tuplas.py
@@ -0,0 +1,10 @@
+# Definir una tupla de colores
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+# Imprimir el último elemento de la tupla usando índice negativo
+print(colores[-1])# Imprime 'naranja'
+
+# Definir una tupla de números
+numeros = (10, 1, 5, 11)
+# Realizar una operación con los valores de la tupla
+operacion=numeros[0]+numeros[2]+numeros[-1]-numeros[1]
+print(operacion)# Imprimir el resultado de la operación
