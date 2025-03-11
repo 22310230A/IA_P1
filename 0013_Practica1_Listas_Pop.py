ea917b9e0a7f96e2b6d5eb5cbcd71a7836e90603
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 21:49:15 -0600

    Capitulo 13
    pop(índice): Se usa para eliminar y devolver un elemento de la lista.
    Eliminaciones:
    
        colores.pop(1) elimina 'azul'.
        colores.pop(-2) elimina 'blanco'.
    
    print(colores): Se muestra la lista sin los elementos eliminados.

---
+++ 0013_Practica1_Listas_Pop.py
@@ -0,0 +1,11 @@
+# Definir una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Eliminar y almacenar el segundo elemento (índice 1 → 'azul')
+eliminar1= colores.pop(1)
+
+# Eliminar y almacenar el penúltimo elemento (índice -2 → 'blanco')
+eliminar2= colores.pop(-2)
+
+# Imprimir la lista después de las eliminaciones
+print(colores)
