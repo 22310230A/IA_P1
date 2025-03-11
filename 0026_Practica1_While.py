dc37de3f9acf4292df0e25ad5fd95c015965e96f
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:59:58 -0600

    Capitulo 26
    Primer while: El bucle incrementa x de 5 en 5 y lo imprime hasta que x sea mayor que 15.
    
    Segundo while: El bucle decrementa x en 20 hasta que x sea menor que -100, mostrando el valor de x en cada iteración.
    
    Tercer while: Imprime el valor de x desde 10 hasta 0, y decrementa x en 1 en cada iteración.

---
+++ 0026_Practica1_While.py
@@ -0,0 +1,18 @@
+# Inicializar la variable x
+x = 0
+# Primer bucle while: incrementa x en 5 hasta que x sea mayor que 15
+while x <= 15:
+	print(x)# Imprime el valor de x
+	x += 5 # Incrementa x en 5 en cada iteración
+
+
+# Segundo bucle while: decrementa x en 20 hasta que x sea menor que -100
+while x >= -100:
+	print(x) # Imprime el valor de x
+	x -= 20 # Decrementa x en 20 en cada iteración
+
+# Tercer bucle while: imprime el valor de x y lo decrementa en 1 hasta llegar a 0	
+x=10
+while x >= 0:
+	print('El valor del bucle es:',x)# Imprime el valor de x
+	x -= 1  # Decrementa x en 1 en cada iteración
