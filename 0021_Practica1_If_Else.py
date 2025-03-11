27037b1e6471fb29906552ed5dd7097445ce749a
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:33:33 -0600

    Capitulo 21
    Operador == (igualdad): Comprueba si color es igual a "rojo".
    
    if se ejecuta si la condición es verdadera.
    
    else se ejecutaría si la condición fuera falsa.

---
+++ 0021_Practica1_If_Else.py
@@ -0,0 +1,7 @@
+# Definir una variable con un color
+color = "rojo"
+# Verificar si el color es "rojo"
+if color == "rojo":
+    print("El color es rojo")# Se ejecuta porque color es "rojo"
+else:
+    print("El color no es rojo")# No se ejecuta en este caso
