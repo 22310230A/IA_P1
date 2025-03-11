c27df9915c80f3aa3aef0c7e9608cc55827d2bea
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 21:54:22 -0600

    Capitulo 14
    append(valor): Se usa para agregar un elemento al final de la lista.
    Elementos añadidos:
    
        colores.append("fuxia") agrega 'fuxia' al final.
        colores.append("celeste") agrega 'celeste' al final.
    
    print(colores): Se muestra la lista con los nuevos colores.

---
+++ 0014_Practica1_Listas_append.py
@@ -0,0 +1,11 @@
+# Definir una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Agregar 'fuxia' al final de la lista
+colores.append("fuxia")
+
+# Agregar 'celeste' al final de la lista
+colores.append("celeste")
+
+# Imprimir la lista con los nuevos colores añadidos
+print(colores)
