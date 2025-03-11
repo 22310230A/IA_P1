67275bf40ebb1ff81f58d830600ec88a111cd52e
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:03:02 -0600

    Capitulo 16
    sort(reverse=True): Ordena la lista en orden alfabético inverso (de la Z a la A).
    print(colores): Muestra la lista después de la ordenación descendente.

---
+++ 0016_Practica1_Listas_Sorted_Sort.py
@@ -0,0 +1,9 @@
+# Definir una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón',
+'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Ordenar la lista en orden alfabético inverso (de Z a A)
+colores.sort(reverse=True)
+
+# Imprimir la lista ordenada de forma descendente
+print(colores)
