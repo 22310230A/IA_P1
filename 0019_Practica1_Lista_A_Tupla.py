d6ffc18d3d5b5a61ed30bcb2092e08f2e05f1d49
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:27:11 -0600

    Capitulo 19
    tuple(colores): Convierte la lista colores en una tupla.
    
    type(tuplacolor): Se usa para verificar el tipo de dato de tuplacolor.
    
    El resultado class 'tuple' indica que ahora es una tupla.

---
+++ 0019_Practica1_Lista_A_Tupla.py
@@ -0,0 +1,6 @@
+# Definir una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+# Convertir la lista en una tupla
+tuplacolor=tuple(colores)
+# Imprimir el tipo de dato de la nueva variable
+print(type(tuplacolor))# Imprime: <class 'tuple'>
