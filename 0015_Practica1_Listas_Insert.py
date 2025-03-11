698e1b77c0cd85737ce6d1409e29de969c18c963
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 21:59:20 -0600

    Capitulo 15
    insert(índice, valor): Se usa para insertar un elemento en una posición específica de la lista.
    Inserciones:
    
        colores.insert(-4, "magenta") inserta 'magenta' en la cuarta posición desde el final.
        colores.insert(-1, "turquesa") inserta 'turquesa' en la penúltima posición.
    
    print(colores): Se muestra la lista con los nuevos colores añadidos.

---
+++ 0015_Practica1_Listas_Insert.py
@@ -0,0 +1,11 @@
+# Definir una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Insertar 'magenta' en la cuarta posición desde el final (índice -4)
+colores.insert(-4,"magenta")
+
+# Insertar 'turquesa' en la penúltima posición (índice -1)
+colores.insert(-1,"turquesa")
+
+# Imprimir la lista con los nuevos colores añadidos
+print(colores)
