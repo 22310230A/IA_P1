f2cf3e6eb5f99c532a6cf35742e920b9bb5f3587
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:47:47 -0600

    Capitulo 23
    input(): Pide al usuario ingresar un valor.
    
    in: Verifica si el valor ingresado por el usuario está dentro de la tupla.
    
    if-else: Compara el valor ingresado y ejecuta el bloque correspondiente según si está o no en la tupla.

---
+++ 0023_Practica1_Buscar_Datos_Listas_Tuplas.py
@@ -0,0 +1,10 @@
+
+# Definir una tupla con colores
+tupla=("cafe","negro","rosa","morado")
+# Solicitar al usuario que ingrese un color
+colores=input("Ingresa un color:")
+# Verificar si el color ingresado está en la tupla
+if colores in tupla:
+    print("El color esta en la tupla")# Se ejecuta si el color ingresado está en la tupla
+else :
+    print("El color no esta en la tupla")# Se ejecuta si el color ingresado no está en la tupla
