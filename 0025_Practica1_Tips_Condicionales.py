cb18fbaa6a682cbf5b9ff38bd4daf1cf7e9af4b7
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:50:12 -0600

    Capitulo 25
    if: Se evalúa si x es menor que y.
    print(): Si la condición es verdadera, imprime el mensaje 'x es menor que y.'.

---
+++ 0025_Practica1_Tips_Condicionales.py
@@ -0,0 +1,7 @@
+# Asignación de valores a las variables
+x = 100
+y = 200
+
+# Verificar si x es menor que y y mostrar el mensaje correspondiente
+if x < y:  # Si x es menor que y
+    print('x es menor que y.')  # Imprime el mensaje
