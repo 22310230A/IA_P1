f4090dee53f2bbf0adc7b8341d78f953cc9e3a5c
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 23:11:31 -0600

    Capitulo 28
    Tupla colores: La tupla contiene los colores 'rojo', 'azul', 'verde' y 'amarillo'.
    
    Bucle for: Se recorre cada elemento (color) de la tupla.
    
    print(): En cada iteración, se imprime el mensaje "El color es: " seguido del color actual de la tupla, con un punto al final.

---
+++ 0028_Practica1_For.py
@@ -0,0 +1,5 @@
+# Definir una tupla con algunos colores
+colores = ('rojo','azul','verde','amarillo')
+# Bucle for que recorre cada color en la tupla
+for x in colores:
+    print('El color es: ' ,x ,'.')# Imprimir el mensaje con el color actual
