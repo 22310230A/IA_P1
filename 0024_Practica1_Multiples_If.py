e88f78baa1ecf680f05793870ade8e2b5b20201c
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:48:57 -0600

    Capitulo 24
    input(): Solicita al usuario que ingrese un número.
    int(): Convierte el valor ingresado a un número entero.
    if: Se usan múltiples sentencias if para verificar cuál opción ha seleccionado el usuario y ejecutar el bloque correspondiente.

---
+++ 0024_Practica1_Multiples_If.py
@@ -0,0 +1,12 @@
+# Solicitar un número entre 1 y 4 al usuario
+entrada = int(input('Introduce un número del 1 al 4:\n'))
+
+# Verificar la opción seleccionada y mostrar el mensaje correspondiente
+if entrada == 1:  # Si el número ingresado es 1
+    print('Has seleccionado la opción número 1.')
+if entrada == 2:  # Si el número ingresado es 2
+    print('Has seleccionado la opción número 2.')
+if entrada == 3:  # Si el número ingresado es 3
+    print('Has seleccionado la opción número 3.')
+if entrada == 4:  # Si el número ingresado es 4
+    print('Has seleccionado la opción número 4.')
