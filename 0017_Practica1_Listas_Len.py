7a7fd97b4333d2ee185f6aa0a2340eb987cfb0de
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:06:13 -0600

    Capitulo 17
    len(lista): Se usa para obtener la cantidad de elementos en una lista.
    longitud_colores almacena el número total de elementos en colores.
    print(longitud_colores) mostraría 4, ya que la lista tiene 4 elementos.

---
+++ 0017_Practica1_Listas_Len.py
@@ -0,0 +1,8 @@
+# Definir una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+
+# Obtener la cantidad de elementos en la lista
+longitud_colores = len(colores)
+
+# Imprimir la cantidad de elementos en la lista
+print(longitud_colores)
