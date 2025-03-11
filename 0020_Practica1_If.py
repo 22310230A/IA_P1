cb48535d95bb6b0ad983ef68e831ee6bf521dcc9
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-10 22:30:20 -0600

    Capitulo 20
    Operador != (diferente): Verifica si dos valores son distintos.
    
    Operador > (mayor que): Comprueba si un número es mayor que otro.
    if se ejecuta solo si la condición es verdadera.
    
    Se imprimen dos mensajes porque solo dos condiciones son verdaderas.

---
+++ 0020_Practica1_If.py
@@ -0,0 +1,20 @@
+# Definir dos números
+num1 = 15
+num2 = 20
+# Verificar si los números son diferentes
+if num1 != num2:
+	print('Se ejecuta el if.')# Se ejecuta porque 15 es diferente de 20
+
+# Cambiar valores de las variables	
+num1 = 1450
+num2 = 60
+# Verificar si num1 es mayor que num2
+if num1  > num2:
+	print('Se ejecuta el if.') # Se ejecuta porque 1450 es mayor que 60
+	
+# Cambiar valores de las variables    
+num1 = 60
+num2 = 60
+# Verificar si los números son diferentes
+if num1 != num2:
+	print('Se ejecuta el if.')# No se ejecuta porque 60 es igual a 60
