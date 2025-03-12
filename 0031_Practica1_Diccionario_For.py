60f8bb41f5177f81c004f886fb06b5a391209c95
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-11 21:13:07 -0600

    Capitulo 31
    Estructura de datos: Se definen dos diccionarios (teclado1 y teclado2) con información sobre teclados, incluyendo categoría, modelo y precio.
    Bucle for:
    
        Recorre cada clave del diccionario teclado2.
        En cada iteración, se imprime la clave (x) seguida del valor correspondiente (teclado2[x]).

---
+++ 0031_Practica1_Diccionario_For.py
@@ -0,0 +1,16 @@
+# Diccionario 'teclado1' con información sobre un teclado			
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+# Diccionario 'teclado2' con información sobre otro teclado
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+# Bucle for para recorrer las claves del diccionario 'teclado2'
+for x in teclado2:
+	print(x,"=",teclado2[x],".")
+	
