5ad9a5bb7807e4dfe41528314fa0f4619b794252
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-11 21:19:27 -0600

    Capitulo 32
    Se definen dos diccionarios (teclado1 y teclado2) con información de teclados.
    
    del teclado1: Borra completamente el diccionario teclado1 de la memoria.
    
    del teclado2['Categoría']: Elimina la clave 'Categoría' del diccionario teclado2.
    
    del teclado2["Precio"]: Elimina la clave 'Precio' del diccionario teclado2.
    
    print(teclado2): Muestra el diccionario después de las eliminaciones.

---
+++ 0032_Practica1_Metodos_Diccionario.py
@@ -0,0 +1,23 @@
+# Diccionario 'teclado1' con información sobre un teclado
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+# Diccionario 'teclado2' con información sobre otro teclado
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+# Elimina completamente el diccionario 'teclado1' de la memoria
+del teclado1
+
+# Elimina la clave 'Categoría' del diccionario 'teclado2'
+del teclado2['Categoría']
+
+# Elimina la clave 'Precio' del diccionario 'teclado2'
+del teclado2["Precio"]
+
+# Imprime el diccionario restante
+print(teclado2)
