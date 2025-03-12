d27b508d25e3b35ac0735116a54e2476c6ad0b55
JonaBN <143145351+22310230A@users.noreply.github.com>
2025-03-11 21:32:56 -0600

    Capitulo 33
    Definición de función: Se define una función llamada suma que recibe dos números como parámetros.
    Cálculo de la suma: Se suman los dos números y el resultado se almacena en la variable sumaf.
    Impresión de resultados: Se imprimen tres resultados en pantalla:
    
        La suma original.
        La suma original más 20.
        La suma original más 56,970.
    
    Llamado a la función: Se llama a la función suma(15,15), lo que genera e imprime los resultados correspondientes.

---
+++ 0033_Practica1_Funciones.py
@@ -0,0 +1,11 @@
+# Definimos una función llamada 'suma' que recibe dos números como argumentos
+def suma(numero1,numero2):
+    # Se calcula la suma de los dos números y se almacena en 'sumaf'
+    sumaf=numero1+numero2
+     # Se imprimen tres resultados diferentes:
+    print("El resultado de la primera suma es:",sumaf)# Imprime la suma normal
+    print("El resultado de la primera suma es:",sumaf+20)  # Imprime la suma más 20
+    print("El resultado de la primera suma es:",sumaf+56970) # Imprime la suma más 56970
+
+# Llamamos a la función con los valores 15 y 15
+suma(15,15)
